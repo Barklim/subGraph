52c413513406c1ceddf0055eedcad23fd4b1bb93
Barklim <46284548+Barklim@users.noreply.github.c>
2023-07-18 03:45:40 +0800

    codegen

--- src/token.ts
+++ src/token.ts
@@ -1,91 +1,40 @@
 import {
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
-  TokenMetadataURIUpdated as TokenMetadataURIUpdatedEvent,
   TokenURIUpdated as TokenURIUpdatedEvent,
-  Transfer as TransferEvent
+  Transfer as TransferEvent,
+  Token as TokenContract
 } from "../generated/Token/Token"
 import {
-  Approval,
-  ApprovalForAll,
-  TokenMetadataURIUpdated,
-  TokenURIUpdated,
-  Transfer
+  Token,
+  User
 } from "../generated/schema"
 
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
+export function handleTransfer(event: TransferEvent): void {
+  let token = Token.load(event.params.tokenId.toString());
+  if (!token) {
+    token = new Token(event.params.tokenId.toString());
+    token.creator = event.params.to.toHexString();
+    token._tokenId = event.params.tokenId;
+    token.createdTimestamp = event.block.timestamp;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+    let tokenContract = TokenContract.bind(event.address);
+    token.contentURI = tokenContract.tokenURI(event.params.tokenId);
+    token.metadataURI = tokenContract.tokenMetadataURI(event.params.tokenId);
+  }
+  token.owner = event.params.to.toHexString()
+  token.save();
 
-  entity.save()
-}
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleTokenMetadataURIUpdated(
-  event: TokenMetadataURIUpdatedEvent
-): void {
-  let entity = new TokenMetadataURIUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._tokenId = event.params._tokenId
-  entity.owner = event.params.owner
-  entity._uri = event.params._uri
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let user = User.load(event.params.to.toHexString());
+  if (!user) {
+    user = new User(event.params.to.toHexString());
+    user.save();
+  }
 }
 
 export function handleTokenURIUpdated(event: TokenURIUpdatedEvent): void {
-  let entity = new TokenURIUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._tokenId = event.params._tokenId
-  entity.owner = event.params.owner
-  entity._uri = event.params._uri
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let token = Token.load(event.params._tokenId.toString());
+  if (token != null) {
+    token.contentURI = event.params._uri;
+    token.save()
+  }
 }
 
-export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
